72cf7cc7b5b84dc8d8797d4796c1e483835d371a
zjpiazza <zjpiazza@gmail.com>
2025-09-08 13:18:56 -0500

    Sync

--- app/src/types/collection-meta-extensions.d.ts
+++
@@ -1,8 +0,0 @@
-import type { CollectionMeta } from '@directus/types';
-
-declare module '@directus/types' {
-	interface CollectionMeta {
-		custom_item_component?: string | null;
-		custom_header_component?: string | null;
-	}
-}
